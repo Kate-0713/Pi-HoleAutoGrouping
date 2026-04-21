e38d795f4066220af6a048eef81643c9b7d73e38
Kate <37121755+Kate-0713@users.noreply.github.com>
2026-04-22 00:32:46 +0100

    Merged in already written code
    Merged in code from the local project folder and updated README to include TODO list

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pi-HoleAutoGrouping
 Move Pi-Hole clients into certain groups based on a given prefix in the client comment
+
+
+# TODO
+- Allow matching groups against a prefix rather than just the group ID
+- Properly package script for easy installation of dependencies (notably the requests library)
+- Create bash script to pull files and schedule script to run periodically
+- Add logging to script
---
+++ code-and-config/main.py
@@ -0,0 +1,66 @@
+import json
+import requests
+import sys
+
+
+def get_all_clients():  # Gets a list of all known clients
+    url = f"{BASE_URL}/clients"
+    response = requests.request("GET",url, headers=HEADERS)
+    response.raise_for_status()
+    return response.json()
+
+def name_matches_prefix(name):  # Checks to see if the comment section of a client starts with a given string
+    return any(name.get("comment").startswith(prefix) for prefix in TARGET_PREFIXES)
+
+def reset_clients_with_prefixes():  # main function that performs the client sorting and sends the update request
+    clients_json = get_all_clients()  # retrieves list of all known clients and stores it
+
+    clients_list = clients_json.get("clients")
+    client_id_list = []
+    for item in clients_list:  # iterate over all clients and store them in a separate list if the comment starts with a certain prefix
+        client_id_list.append(item) if name_matches_prefix(item) else print(f"Skipping {item.get("comment")}")
+
+
+    for client in client_id_list:  # update client groups of every client that got moved into the second list
+        response = requests.request("PUT", f"{BASE_URL}/clients/{client.get("client")}", headers=HEADERS,
+                                    json={"client": f"{client.get("client")}", "name": f"{client.get("name")}",
+                                    "comment": f"{client.get("comment")}", "groups": TARGET_GROUPS})
+        response.raise_for_status()
+
+    print("Clients with matching prefixes have been reset.")
+    return
+
+def get_auth():  # Authenticates with Pi-Hole and gets session ID
+    response = requests.request("POST", f"{BASE_URL}/auth", json={"password": f"{API_TOKEN}"})
+    response.raise_for_status()
+    return response.json().get("session").get("sid")
+
+def show_groups():  # Only used during testing
+    response = requests.request("GET", f"{BASE_URL}/groups", headers=HEADERS, json={"password": f"{API_TOKEN}"})
+    response.raise_for_status()
+    group_info_list = []
+    for x in response.json().get("groups"):
+        group_info_list.append(f"{x.get("name")} , {x.get("id")}")
+    return group_info_list
+
+
+if __name__ == "__main__":
+
+    with open("config.json", "r") as f:
+        env_file = json.load(f)
+    f.close()
+
+    BASE_URL = env_file.get("BASE_URL")  # URL to add api enpoints to
+    API_TOKEN = env_file.get("API_TOKEN")  # Pi-Hole app password
+    TARGET_PREFIXES = env_file.get("TARGET_PREFIXES")  # Prefixes to search for
+    TARGET_GROUPS = env_file.get("TARGET_GROUPS")  # Group IDs to move clients into
+
+    sid = get_auth()
+    HEADERS = {"X-FTL-SID": f"{sid}", "Accept": "application/json", "Content-Type": "application/json"}
+
+    # uncomment to show group IDs in console
+    # print(show_groups())
+    # sys.exit(0)
+
+    reset_clients_with_prefixes()
+
